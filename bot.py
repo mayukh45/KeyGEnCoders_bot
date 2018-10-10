b444590896692460011fa825f9c13cb2cc85a6a1
Mayukh Sengupta <mayukh1110@gmail.com>
2018-10-11 00:22:26 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,31 @@
+import discord
+import discord.user as user
+test_id = 499296940358369280
+
+client = discord.Client()
+print(discord.version_info)
+
+
+
+@client.event
+async def on_message(message):
+    print(":>")
+
+    await client.send_message(message.channel, 'Why')
+
+    print(type(client.messages))
+
+
+
+@client.event
+async def on_ready():
+
+    print('Logged in as')
+    print(client.guilds)
+    users =  list(client.users)
+    for users in users:
+        print(users.name)
+    print(client.get_channel(test_id))
+
+
+client.run(os.getenv('TOKEN'))
